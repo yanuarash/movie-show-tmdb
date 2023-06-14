720c382ecd2ba79b962230d5ca71a8387e51477f
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:17:51 +0700

    add usecase for home module

---
+++ app/src/main/java/com/example/movieshowtmdb/modules/home/usecase/GenresUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.movieshowtmdb.modules.home.usecase
+
+import com.example.kotlintesttmdb.models.Genres
+import com.example.kotlintesttmdb.network.Resource
+import com.example.movieshowtmdb.modules.home.repo.GenresRepo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class GenresUseCase constructor(private val genresRepo: GenresRepo) {
+    operator fun invoke(language: String): Flow<Resource<Genres>> = flow{
+        try {
+            emit(Resource.Loading())
+            val res = genresRepo.getGenres(language)
+            emit(Resource.Success(res))
+        }
+        catch (e: HttpException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+        catch (e: IOException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+    }
+}
