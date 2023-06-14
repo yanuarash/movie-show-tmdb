72c435140b6061c9b8c6fc1f0d3e7afdda09f5a7
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:37:05 +0700

    add mandatory functional for navigation

--- app/src/main/java/com/example/movieshowtmdb/MainActivity.kt
+++ app/src/main/java/com/example/movieshowtmdb/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.movieshowtmdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.kotlintesttmdb.pages.NavigationPages
 import com.example.movieshowtmdb.ui.theme.MovieShowTMDBTheme
-
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +27,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navHostController = rememberNavController()
+                    NavigationPages(navHostController = navHostController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieShowTMDBTheme {
-        Greeting("Android")
     }
 }
