6f953b223fd6aa74444a95a57b93c9b1cf0347d6
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:17:18 +0700

    add models for home modules

---
+++ app/src/main/java/com/example/movieshowtmdb/modules/home/models/Genres.kt
@@ -0,0 +1,7 @@
+package com.example.kotlintesttmdb.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Genres (
+    val genres: List<GenresItem>,
+    )
---
+++ app/src/main/java/com/example/movieshowtmdb/modules/home/models/GenresItem.kt
@@ -0,0 +1,8 @@
+package com.example.kotlintesttmdb.models
+
+import com.google.gson.annotations.SerializedName
+
+data class GenresItem(
+    val id: Int,
+    val name: String
+)
