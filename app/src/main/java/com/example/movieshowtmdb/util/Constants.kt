3de3feb75417309f5ab9ee81b53a01a34219cd88
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:17:03 +0700

    add constants

---
+++ app/src/main/java/com/example/movieshowtmdb/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.movieshowtmdb.util
+
+object Constants{
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "37543ba18b77d82e1164ae363641f2d9"
+    const val IMAGE_W1280_URL = "https://image.tmdb.org/t/p/w1280/"
+    const val YOUTUBE_THUMBNAIL = "https://img.youtube.com/vi/"
+    const val YOUTUBE_THUMBNAIL_RES = "/maxresdefault.jpg"
+    const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
+}
