48e3f326aae15c40e99b59adae8290fbf3daa00c
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:44:35 +0700

    add usecase for movie review module

---
+++ app/src/main/java/com/example/movieshowtmdb/modules/movies_reviews/usecase/MoviesReviewsUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.movieshowtmdb.modules.movies_reviews.usecase
+
+import com.example.kotlintesttmdb.models.MoviesReviews
+import com.example.kotlintesttmdb.network.Resource
+import com.example.kotlintesttmdb.network.repo.MoviesReviewsRepo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class MoviesReviewsUseCase constructor(private val moviesGenreRepo: MoviesReviewsRepo) {
+    operator fun invoke(movieId: Int, page: Int): Flow<Resource<MoviesReviews>> = flow{
+        try {
+            emit(Resource.Loading())
+            val res = moviesGenreRepo.getMoviesReviews(movieId, page)
+            emit(Resource.Success(res))
+        }
+        catch (e: HttpException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+        catch (e: IOException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+    }
+}
