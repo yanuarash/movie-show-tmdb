2bd1db310a4604d68a3aa06467258241ccc0b27c
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:00:26 +0700

    add repo for movie videos module

---
+++ app/src/main/java/com/example/movieshowtmdb/modules/movies_videos/repo/MoviesVideosRepo.kt
@@ -0,0 +1,7 @@
+package com.example.kotlintesttmdb.network.repo
+
+import com.example.kotlintesttmdb.models.MoviesVideos
+
+interface MoviesVideosRepo{
+    suspend fun getMoviesVideos(movieId: Int): MoviesVideos
+}
---
+++ app/src/main/java/com/example/movieshowtmdb/modules/movies_videos/repo/MoviesVideosRepoImpl.kt
@@ -0,0 +1,11 @@
+package com.example.kotlintesttmdb.network.repo
+
+import com.example.kotlintesttmdb.models.MoviesVideos
+import com.example.kotlintesttmdb.network.ApiRequest
+import com.example.kotlintesttmdb.network.ApiService
+
+class MoviesVideosRepoImpl constructor(private val apiService: ApiService): MoviesVideosRepo, ApiRequest(){
+    override suspend fun getMoviesVideos(movieId: Int): MoviesVideos {
+        return apiRequest { apiService.getMoviesVideos(movieId) }
+    }
+}
