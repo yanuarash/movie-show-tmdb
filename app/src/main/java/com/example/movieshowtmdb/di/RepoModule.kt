ff610d541820ac03366aa2c8dfa0d691f43f1b52
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:51:01 +0700

    add movie review di

--- app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
@@ -4,11 +4,14 @@ import com.example.kotlintesttmdb.network.repo.MoviesDetailRepo
 import com.example.kotlintesttmdb.network.repo.MoviesDetailRepoImpl
 import com.example.kotlintesttmdb.network.repo.MoviesGenreRepo
 import com.example.kotlintesttmdb.network.repo.MoviesGenreRepoImpl
+import com.example.kotlintesttmdb.network.repo.MoviesReviewsRepo
+import com.example.kotlintesttmdb.network.repo.MoviesReviewsRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenreRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenresRepo
 import com.example.movieshowtmdb.modules.home.usecase.GenresUseCase
 import com.example.movieshowtmdb.modules.movies_detail.usecase.MoviesDetailUseCase
 import com.example.movieshowtmdb.modules.movies_genre.usecase.MoviesGenreUseCase
+import com.example.movieshowtmdb.modules.movies_reviews.usecase.MoviesReviewsUseCase
 import org.koin.dsl.module
 
 val repoModule = module {
@@ -18,4 +21,6 @@ val repoModule = module {
     single<MoviesGenreRepo> { MoviesGenreRepoImpl(get()) }
     single { MoviesDetailUseCase(get()) }
     single<MoviesDetailRepo> { MoviesDetailRepoImpl(get()) }
+    single { MoviesReviewsUseCase(get()) }
+    single<MoviesReviewsRepo> { MoviesReviewsRepoImpl(get()) }
 }
--- app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.example.kotlintesttmdb.di
 
 import com.example.kotlintesttmdb.pages.movies_detail.MoviesDetailViewModel
 import com.example.kotlintesttmdb.pages.movies_genre.MoviesGenreViewModel
+import com.example.kotlintesttmdb.pages.movies_reviews.MoviesReviewsViewModel
 import com.example.movieshowtmdb.modules.home.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -10,4 +11,5 @@ val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { MoviesGenreViewModel(get()) }
     viewModel { MoviesDetailViewModel(get()) }
+    viewModel { MoviesReviewsViewModel(get()) }
 }
