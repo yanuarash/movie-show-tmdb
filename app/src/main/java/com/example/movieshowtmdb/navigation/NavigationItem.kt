4d13e4f9c56a25cb197f0fddb5d18c5dcb882160
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:41:25 +0700

    add navigation movie detail module

--- app/src/main/java/com/example/movieshowtmdb/navigation/NavigationItem.kt
+++ app/src/main/java/com/example/movieshowtmdb/navigation/NavigationItem.kt
@@ -3,4 +3,6 @@ package com.example.kotlintesttmdb.navigation
 sealed class NavigationItem (var route: String, var title: String){
     object Home : NavigationItem("home", "Home")
     object MoviesGenre : NavigationItem("moviesGenre/{withGenres}", "Movies Genre")
+    object MoviesDetail : NavigationItem("moviesDetail/{movieId}", "Movies Detail");
+
 }
--- app/src/main/java/com/example/movieshowtmdb/navigation/NavigationPages.kt
+++ app/src/main/java/com/example/movieshowtmdb/navigation/NavigationPages.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.kotlintesttmdb.navigation.NavigationItem
 import com.example.movieshowtmdb.modules.home.pages.Home
+import com.example.movieshowtmdb.modules.movies_detail.pages.MoviesDetail
 import com.example.movieshowtmdb.modules.movies_genre.pages.MoviesGenre
 
 @ExperimentalMaterial3Api
@@ -24,6 +25,11 @@ fun NavigationPages(navHostController: NavHostController){
         composable(NavigationItem.MoviesGenre.route, arguments = listOf(navArgument("withGenres"){type = NavType.StringType})){
             val withGenres  = it.arguments?.getString("withGenres")
             MoviesGenre(withGenres = withGenres!!, navHostController = navHostController)
+        }
+
+        composable(NavigationItem.MoviesDetail.route, arguments = listOf(navArgument("movieId"){type = NavType.StringType})){
+            val movieId  = it.arguments?.getString("movieId")?.toInt()
+            MoviesDetail(movieId = movieId!!, navHostController = navHostController)
         }
     }
 }
