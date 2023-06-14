58b910a2302f48101919e126f5244d02f2a6c533
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 19:33:26 +0700

    add helper functions

---
+++ app/src/main/java/com/example/movieshowtmdb/util/Helper.kt
@@ -0,0 +1,36 @@
+package com.example.movieshowtmdb.util
+
+import com.example.movieshowtmdb.modules.home.models.GenresItem
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+fun showMovieYear(date: String): String? {
+    val parser = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    val formatter = SimpleDateFormat("yyyy", Locale.US)
+    return parser.parse(date)?.let { formatter.format(it) }
+}
+
+fun showMovieDate(date: String): String? {
+    val parser = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    val formatter = SimpleDateFormat("dd MMM yyyy", Locale.US)
+    return parser.parse(date)?.let { formatter.format(it) }
+}
+
+fun roundFloorDecimal(number: Double): Double {
+    val df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.FLOOR
+    return df.format(number).toDouble()
+}
+
+fun listOfGenres(genres: List<GenresItem>): String {
+    if (genres.size == 1) {
+        return genres[0].name
+    }
+    val sb = StringBuilder()
+    for (genre in genres) {
+        sb.append("${genre.name}, ")
+    }
+    return sb.toString()
+}
