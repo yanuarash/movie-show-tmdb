f7f55ff6c37507331d8a5089e25a9973eda43b86
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:18:48 +0700

    remove unnecesssary import

--- app/src/main/java/com/example/movieshowtmdb/networking/ApiService.kt
+++ app/src/main/java/com/example/movieshowtmdb/networking/ApiService.kt
@@ -1,11 +1,9 @@
 package com.example.kotlintesttmdb.network
 
-import com.example.kotlintesttmdb.helper.Constants
 import com.example.kotlintesttmdb.models.*
 import com.example.movieshowtmdb.util.Constants
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
