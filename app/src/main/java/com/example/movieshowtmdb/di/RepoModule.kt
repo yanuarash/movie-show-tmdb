6146b7777d95c70099cf7df6ee6a87a57761d0fb
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:50:20 +0700

    add movie detail di

--- app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
@@ -1,10 +1,13 @@
 package com.example.kotlintesttmdb.di
 
+import com.example.kotlintesttmdb.network.repo.MoviesDetailRepo
+import com.example.kotlintesttmdb.network.repo.MoviesDetailRepoImpl
 import com.example.kotlintesttmdb.network.repo.MoviesGenreRepo
 import com.example.kotlintesttmdb.network.repo.MoviesGenreRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenreRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenresRepo
 import com.example.movieshowtmdb.modules.home.usecase.GenresUseCase
+import com.example.movieshowtmdb.modules.movies_detail.usecase.MoviesDetailUseCase
 import com.example.movieshowtmdb.modules.movies_genre.usecase.MoviesGenreUseCase
 import org.koin.dsl.module
 
@@ -13,4 +16,6 @@ val repoModule = module {
     single<GenresRepo> { GenreRepoImpl(get()) }
     single { MoviesGenreUseCase(get()) }
     single<MoviesGenreRepo> { MoviesGenreRepoImpl(get()) }
+    single { MoviesDetailUseCase(get()) }
+    single<MoviesDetailRepo> { MoviesDetailRepoImpl(get()) }
 }
--- app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.example.kotlintesttmdb.di
 
+import com.example.kotlintesttmdb.pages.movies_detail.MoviesDetailViewModel
 import com.example.kotlintesttmdb.pages.movies_genre.MoviesGenreViewModel
 import com.example.movieshowtmdb.modules.home.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -7,5 +8,6 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
-    viewModel{ MoviesGenreViewModel(get()) }
+    viewModel { MoviesGenreViewModel(get()) }
+    viewModel { MoviesDetailViewModel(get()) }
 }
