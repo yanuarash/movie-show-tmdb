41d61ff3f9baea437692c378d8b3cb7cd9c2d746
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 15:49:40 +0700

    add movie genre di

--- app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/RepoModule.kt
@@ -1,11 +1,16 @@
 package com.example.kotlintesttmdb.di
 
+import com.example.kotlintesttmdb.network.repo.MoviesGenreRepo
+import com.example.kotlintesttmdb.network.repo.MoviesGenreRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenreRepoImpl
 import com.example.movieshowtmdb.modules.home.repo.GenresRepo
 import com.example.movieshowtmdb.modules.home.usecase.GenresUseCase
+import com.example.movieshowtmdb.modules.movies_genre.usecase.MoviesGenreUseCase
 import org.koin.dsl.module
 
 val repoModule = module {
     single { GenresUseCase(get()) }
     single<GenresRepo> { GenreRepoImpl(get()) }
+    single { MoviesGenreUseCase(get()) }
+    single<MoviesGenreRepo> { MoviesGenreRepoImpl(get()) }
 }
--- app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
+++ app/src/main/java/com/example/movieshowtmdb/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.example.kotlintesttmdb.di
 
+import com.example.kotlintesttmdb.pages.movies_genre.MoviesGenreViewModel
 import com.example.movieshowtmdb.modules.home.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
+    viewModel{ MoviesGenreViewModel(get()) }
 }
