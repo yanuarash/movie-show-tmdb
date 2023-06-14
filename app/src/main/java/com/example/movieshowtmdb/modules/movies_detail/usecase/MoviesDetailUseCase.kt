d9a31c5ce2636026eb64db1aa33b7748fbe38ec5
Mohammad Yanuar <yanuar.ash@gmail.com>
2023-06-14 14:54:31 +0700

    add usecase for movie detail module

---
+++ app/src/main/java/com/example/movieshowtmdb/modules/movies_detail/usecase/MoviesDetailUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.movieshowtmdb.modules.movies_detail.usecase
+
+import com.example.kotlintesttmdb.network.Resource
+import com.example.kotlintesttmdb.network.repo.MoviesDetailRepo
+import com.example.movieshowtmdb.modules.movies_detail.models.MoviesDetail
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class MoviesDetailUseCase constructor(private val moviesDetailRepo: MoviesDetailRepo) {
+    operator fun invoke(movieId: Int): Flow<Resource<MoviesDetail>> = flow{
+        try {
+            emit(Resource.Loading())
+            val res = moviesDetailRepo.getMoviesDetail(movieId)
+            emit(Resource.Success(res))
+        }
+        catch (e: HttpException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+        catch (e: IOException){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+    }
+}
